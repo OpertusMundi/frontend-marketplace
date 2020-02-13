c1be66dda095eb96bb2962a7e8e19d64e96d306b
Yannis Kouvaras <yannis.kouvaras@kupa.gr>
2020-02-13 17:39:42 +0200

    Configure Vue build output paths

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,11 @@
 module.exports = {
-  publicPath: '/',
+  // Move all assets under a single directory
   assetsDir: 'assets',
+  // Update output directory for production build to add static assets to the final jar/war file
+  outputDir: process.env.NODE_ENV === 'development' ? 'dist' : '../../../target/frontend',
+  // For production build, index.html is treated as a Spring Thymeleaf template
+  indexPath: process.env.NODE_ENV === 'development' ? 'index.html' : '../../src/main/resources/templates/index.html',
+  // For development, proxy all action requests
   devServer: {
     port: 8080,
     proxy: process.env.NODE_ENV === 'development' ? {
