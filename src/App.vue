82232df4f896d6daab77a9a995dfb2ee6b8d3b8b
Yannis Kouvaras <yannis.kouvaras@kupa.gr>
2020-02-13 18:04:29 +0200

    Add Open API Documentation link

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,34 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
+      <router-link to="/">Home</router-link>
+      {{ "|" }}
       <router-link to="/about">About</router-link>
       <template v-if="$store.getters.hasRole('ROLE_USER')">
-        | <router-link to="/user">User</router-link>
+        {{ "|" }}
+        <router-link to="/user">User</router-link>
       </template>
-
       <template v-if="$store.getters.hasRole('ROLE_ADMIN')">
-        | <router-link to="/admin">Admin</router-link>
+        {{ "|" }}
+        <router-link to="/admin">Admin</router-link>
       </template>
+      {{ "|" }}
+      <a v-bind:href="apiUrl" target="_blank">
+        <img class="logo-image" src="./assets/oas.svg" alt="OPERTUS MUNDI API documentation" />
+      </a>
     </div>
     <router-view />
   </div>
 </template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class App extends Vue {
+  apiUrl = `${process.env.VUE_APP_API_GATEWAY_URL}/swagger-ui/api-docs.html`;
+}
+</script>
 
 <style lang="scss">
 #app {
@@ -35,5 +50,10 @@
       color: #42b983;
     }
   }
+}
+img.logo-image {
+  height: 19px;
+  position: relative;
+  top: 4px;
 }
 </style>
