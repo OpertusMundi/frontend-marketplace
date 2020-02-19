a7443f3b751e2e370b48b22c4c21dd370254aa72
Yannis Kouvaras <yannis.kouvaras@kupa.gr>
2020-02-19 20:37:29 +0200

    Update Vue CLI configuration
    Declare proxy port as a parameter.
    Added logout actions to proxied URLs.

--- vue.config.js
+++ vue.config.js
@@ -7,9 +7,7 @@ module.exports = {
   indexPath: process.env.NODE_ENV === 'development' ? 'index.html' : '../templates/index.html',
   // For development, proxy all action requests
   devServer: {
-    // If the remote development server is used, do not change port value
-    // All redirects are sent to localhost:8080
-    port: 8080,
+    port: process.env.VUE_APP_SERVER_PORT,
     proxy: process.env.NODE_ENV === 'development' ? {
       '^/action': {
         target: process.env.VUE_APP_API_GATEWAY_URL,
@@ -17,7 +15,7 @@ module.exports = {
         changeOrigin: true,
         cookieDomainRewrite: '',
       },
-      '^/(login|logged-in)': {
+      '^/(login|logged-in|logout|logout-out)': {
         target: process.env.VUE_APP_API_GATEWAY_URL,
         ws: true,
         changeOrigin: true,
