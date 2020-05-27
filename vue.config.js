169ea4440c7ee8b34fb7e3910e1495cefcd0fec3
Yannis Kouvaras <yannis.kouvaras@kupa.gr>
2020-05-27 17:33:26 +0300

    Update proxy configuration
    Fix post login/logout actions

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,22 @@
 module.exports = {
-  // Move all assets under a single directory
+  /*
+   * Move all assets under a single directory
+   */
   assetsDir: 'assets',
-  // Update output directory for production build to add static assets to the final jar/war file
+  /*
+   * Update output directory for production build to add static assets to the final jar/war file
+   */
   outputDir: process.env.NODE_ENV === 'development' ? 'dist' : '../resources/public',
-  // For production build, index.html is treated as a Spring Thymeleaf template
+  /*
+   * For production build, index.html is treated as a Spring Thymeleaf template
+   *
+   * https://www.thymeleaf.org/documentation.html
+   */
   indexPath: process.env.NODE_ENV === 'development' ? 'index.html' : '../templates/index.html',
-  // For development, proxy all action requests
+  /* For development, proxy all action requests
+   *
+   * https://github.com/chimurai/http-proxy-middleware#http-proxy-options
+   */
   devServer: {
     port: process.env.VUE_APP_SERVER_PORT,
     proxy: process.env.NODE_ENV === 'development' ? {
@@ -16,16 +27,21 @@ module.exports = {
         cookieDomainRewrite: '',
         autoRewrite: true,
       },
-      '^/(login|logged-in|logout|logout-out)': {
+      '^/(login|logged-in|logout|logged-out|login/opertus-mundi|error/*)': {
         target: process.env.VUE_APP_API_GATEWAY_URL,
         ws: true,
         changeOrigin: true,
         cookieDomainRewrite: '',
         /*
-         * Rewrites the location host/port for (301 / 302 / 307 / 308) redirects based on requested host/port. Default: false.
+         * Rewrites the location host/port on (301/302/307/308) redirects based on requested host/port. Default: false.
          * Required for authentication logged-in redirect.
          */
         autoRewrite: true,
+        /*
+         * Rewrites the location protocol on (301/302/307/308) redirects to 'http' or 'https'. Default: null.
+         * Required for authentication logged-out redirect.
+         */
+        protocolRewrite: 'http',
       },
     } : null,
   },
