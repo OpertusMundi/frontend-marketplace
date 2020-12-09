69a801c5ec112beca953195f97a1c661d11e6e54
Yannis Kouvaras <yannis.kouvaras@kupa.gr>
2020-12-09 20:34:16 +0200

    Update configuration model

--- src/model/configuration.ts
+++ src/model/configuration.ts
@@ -5,6 +5,44 @@ export enum EnumAuthProvider {
   OpertusMundi = 'OpertusMundi',
 }
 
+enum EnumDataProfilerSourceType {
+  NETCDF = 'NETCDF',
+  RASTER = 'RASTER',
+  VECTOR = 'VECTOR',
+}
+
+interface AssetFileType {
+  /**
+   * File extensions allowed in a bundle
+   */
+  bundleExtensions: string[] | null;
+  /**
+   * True if multiple files can be uploaded
+   */
+  bundleSupported: boolean;
+  /**
+   * File type category
+   */
+  category: EnumDataProfilerSourceType,
+  /**
+   * Allowed file extensions
+   */
+  extensions: string[] | null;
+  /**
+   * File format
+   */
+  format: string;
+  /**
+   * Optional description of the file type
+   */
+  notes: string;
+}
+
+interface AssetConfiguration {
+  fileTypes: AssetFileType[];
+}
+
 export interface Configuration {
+  asset: AssetConfiguration;
   authProviders: EnumAuthProvider[];
 }
