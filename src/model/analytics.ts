9c975efe573a17cd521f32579a28809657ee4059
Yannis Kouvaras <yannis.kouvaras@kupa.gr>
2021-05-21 10:30:53 +0300

    Add service and model for data analytics

---
+++ src/model/analytics.ts
@@ -0,0 +1,171 @@
+import { EnumTopicCategory } from './catalogue';
+
+export enum EnumTemporalUnit {
+  YEAR = 'YEAR',
+  MONTH = 'MONTH',
+  WEEK = 'WEEK',
+  DAY = 'DAY',
+}
+
+interface TemporalDimension {
+  /**
+   * Time interval unit (required)
+   */
+  unit: EnumTemporalUnit;
+  /**
+   * Min date in YYYY-MM-DD ISO format
+   */
+  min: string;
+  /**
+   * Max date in YYYY-MM-DD ISO format
+   */
+  max: string;
+}
+
+interface SpatialDimension {
+  /**
+   * True if grouping based on country codes must be performed
+   */
+  enabled: boolean;
+  /**
+   * Country codes in ISO 3166-1 alpha-2 format. If one or more codes are
+   * specified, data will be filtered using the specified codes
+   */
+  codes: string[];
+}
+
+interface SegmentDimension {
+  /**
+   * True if grouping based on segment codes must be performed
+   */
+  enabled: boolean;
+  /**
+   * If one or more segments are selected, data will be filtered using the
+   * specified segments
+   */
+  segments: EnumTopicCategory;
+}
+
+interface BaseQuery {
+  /**
+   * Temporal dimension constraints
+   */
+  time: TemporalDimension | null;
+  /**
+   * Spatial dimension constraints
+   */
+  areas: SpatialDimension | null;
+  /**
+   * Segment dimension constraints
+   */
+  segments: SegmentDimension | null;
+  /**
+   * One or more publishers for grouping data. This option is available only to platform
+   * administrators. For providers, the service automatically sets the value to the provider key.
+   */
+  publishers: string[] | null;
+  /**
+   * One or more asset PIDs for grouping data
+   */
+  assets: string[] | null;
+}
+
+export enum EnumSalesQueryMetric {
+  COUNT_TRANSACTIONS = 'COUNT_TRANSACTIONS',
+  SUM_SALES = 'SUM_SALES',
+}
+
+export interface SalesQuery extends BaseQuery {
+  /**
+   * Aggregate to compute
+   */
+  metric: EnumSalesQueryMetric;
+}
+
+export enum EnumAssetSource {
+  SEARCH = 'SEARCH',
+  VIEW = 'VIEW',
+  REFERENCE = 'REFERENCE',
+}
+
+export enum EnumAssetQueryMetric {
+  COUNT = 'COUNT',
+}
+
+export interface AssetQuery extends BaseQuery {
+  /**
+   * Source of data
+   */
+  source: EnumAssetSource;
+  /**
+   * Aggregate to compute
+   */
+  metric: EnumAssetQueryMetric
+}
+
+/**
+ * An object that represents a time instant. Depending on the time unit specified by a
+ * query, properties are set up to the specified time granularity e.g. if time unit is `MONTH`,
+ * only properties `year` and `month` will be returned
+ */
+interface TimeInstant {
+  year: number;
+  month?: number;
+  week?: number;
+  day?: number;
+}
+
+/**
+ * An object that represents a country with its centroid
+ */
+interface Location {
+  /**
+   * Country code in ISO 3166-1 alpha-2 format
+   */
+  code: string;
+  lon?: number;
+  lat?: number;
+}
+
+export interface DataPoint {
+  /**
+   * The time instant the data point refers to. If no temporal constraint is specified in
+   * the query, this property is not set
+   */
+  time?: TimeInstant;
+  /**
+   * The location the data point refers to. If no spatial constraint is specified in the
+   * query, this property is not set
+   */
+  location?: Location;
+  /**
+   * Asset segment
+   */
+  segment?: string;
+  /**
+   * The publisher key the data point refers to. If no publisher constraint is specified
+   * in the query, this property is not set. If a user is not an administrator, this property
+   * is never set
+   */
+  publisher?: string;
+  /**
+   * The asset PID the data point refers to. If no asset constraint is specified in the
+   * query, this property is not set
+   */
+  asset?: string;
+  /**
+   * The value of the data point
+   */
+  value: number;
+}
+
+export interface DataSeries {
+  /**
+   * Request time unit
+   */
+  timeUnit?: EnumTemporalUnit;
+  /**
+   * Data series points
+   */
+  points: DataPoint[];
+}
---
+++ src/service/analytics.ts
@@ -0,0 +1,97 @@
+import Api from '@/service/api';
+
+import { AxiosServerResponse, ServerResponse } from '@/model/response';
+import { SalesQuery, DataSeries } from '@/model/analytics';
+
+export default class AnalyticsApi extends Api {
+  constructor() {
+    super({ withCredentials: true });
+  }
+
+  /**
+   * Executes a query on sales data and returns a single data series.
+   *
+   * Role ROLE_PROVIDER is required.
+   *
+   * @param query Query to execute
+   * @returns
+   *
+   * @example
+   * executeSalesQuery({
+   *   "time": {
+   *     "unit": "WEEK",
+   *     "min": "2021-01-01",
+   *     "max": "2021-12-31"
+   *   },
+   *   "areas": {
+   *     "enabled": true
+   *   },
+   *   "metric": "COUNT_TRANSACTIONS"
+   * });
+   *
+   * // Returns data grouped by date and area. Date instances will
+   * // contain only year and week properties.
+   *
+   * {
+   *   "messages": [],
+   *   "result": {
+   *     "timeUnit": "WEEK",
+   *     "points": [{
+   *       "time": {
+   *         "year": 2021,
+   *         "month": 5,
+   *         "week": 21
+   *       },
+   *       "location": {
+   *         "code": "GR",
+   *         "lon": null,
+   *         "lat": null
+   *       },
+   *       "value": 2
+   *     }]
+   *   },
+   *   "success": true
+   * }
+   */
+  public async executeSalesQuery(query: SalesQuery): Promise<ServerResponse<DataSeries>> {
+    const url = '/action/analytics/sales';
+
+    return this.post<SalesQuery, ServerResponse<DataSeries>>(url, query)
+      .then((response: AxiosServerResponse<DataSeries>) => {
+        const { data } = response;
+
+        return data;
+      });
+  }
+
+  /**
+   * Helper method for converting a data series to a feature collection
+   * @param data
+   * @returns
+   */
+  public toFeatureCollections(data: DataSeries): GeoJSON.FeatureCollection<GeoJSON.Point, { label: string, value: number }> {
+    const result: GeoJSON.FeatureCollection<GeoJSON.Point, { label: string, value: number }> = {
+      type: 'FeatureCollection',
+      features: [],
+    };
+
+    data?.points.forEach((p, index) => {
+      if (p.location?.lon && p.location?.lat && p.location?.code) {
+        result.features.push({
+          id: index,
+          type: 'Feature',
+          geometry: {
+            type: "Point",
+            coordinates: [p.location.lon, p.location.lat],
+          },
+          properties: {
+            label: p.location?.code,
+            value: p.value,
+          }
+        });
+      }
+    });
+
+    return result;
+  }
+}
