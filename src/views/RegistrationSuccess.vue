6fd890c349cc7cacf35aad0e3fbebf77d238b751
treecon <kon3dos@gmail.com>
2021-05-28 12:28:46 +0300

    added registration success/failure redirects

--- src/views/RegistrationSuccess.vue
+++ src/views/RegistrationSuccess.vue
@@ -4,9 +4,10 @@
       <div class="confirm-email__hero">
         <shape></shape>
         <div class="confirm-email__hero__content">
-          <h1 class="confirm-email__hero__content__title">Confirm your email</h1>
+          <h1 class="confirm-email__hero__content__title">Welcome!</h1>
           <div class="confirm-email__hero__content__text">
-            <p>Check your email and click on the link provided.<br>You will be redirected back here automatically.</p>
+            <p>Your email has been confirmed</p>
+            <p class="mt-xs-50"><router-link to="/dashboard" class="btn btn--std btn--teal">GO TO DASHBOARD</router-link></p>
           </div>
       </div>
       </div>
@@ -27,5 +28,8 @@ export default class Login extends Vue {
 }
 </script>
 <style lang="scss">
+  // borrows style from contifm-email
   @import "@/assets/styles/_confirm-email.scss";
+  @import "@/assets/styles/_btns.scss";
+  @import "@/assets/styles/abstracts/_spacings.scss";
 </style>
