ddf99439b5b0f4f1d51b930eb4d506bb70d90696
Tony Durden <antonis@bracketdev.com>
2021-06-22 14:57:59 +0300

    Home 22-06-2021

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -13,33 +13,130 @@
       </div>
     </section>
     <section class="homepage__big_blocks">
-      <div class="s_container">
+         <h4 class="homepage__big_blocks__title" data-aos="fade-up">Unlock the value of your geodata assets with topio!</h4>
+      <div class="m_container">
         <div class="homepage__big_blocks__inner">
           <div class="homepage__big_blocks__item" data-aos="fade-up">
-            <img src="@/assets/images/icons/frictionless.svg" alt="" class="homepage__big_blocks__item__icon">
-            <h3 class="homepage__big_blocks__item__title">Frictionless data</h3>
+            <img src="@/assets/images/icons/home/eu-market.svg" alt="" class="homepage__big_blocks__item__icon">
+            <h3 class="homepage__big_blocks__item__title">An EU Market</h3>
+            <p class="homepage__big_blocks__item__text">
+              Regulatory conformance and security by-design. We generate contracts, clear payments, ensure KYB conformance, and more.
+            </p>
+          </div>
+          <div class="homepage__big_blocks__item" data-aos="fade-up" data-aos-delay="300">
+            <img src="@/assets/images/icons/home/simple-publishing.svg" alt="" class="homepage__big_blocks__item__icon">
+            <h3 class="homepage__big_blocks__item__title">Simple publishing</h3>
+            <p class="homepage__big_blocks__item__text">
+             Publish your data and services in minutes. Add metadata, set your price and terms, and start getting paid.
+            </p>
+          </div>
+          <div class="homepage__big_blocks__item" data-aos="fade-up" data-aos-delay="600">
+            <img src="@/assets/images/icons/home/data-work.svg" alt="" class="homepage__big_blocks__item__icon">
+            <h3 class="homepage__big_blocks__item__title">Let your data work for you</h3>
+            <p class="homepage__big_blocks__item__text">
+             Expand your reach across EU and the world. Tap into new domains and users. Harness value from your existing assets with no effort.
+            </p>
+          </div>
+           <div class="homepage__big_blocks__item" data-aos="fade-up" data-aos-delay="900">
+            <img src="@/assets/images/icons/home/turn-data.svg" alt="" class="homepage__big_blocks__item__icon">
+            <h3 class="homepage__big_blocks__item__title">Turn data to services</h3>
+            <p class="homepage__big_blocks__item__text">
+              We can turn your data into services in a few clicks. Let us do the heavy-lifting and bring you more clients.
+            </p>
+          </div>
+           <div class="homepage__big_blocks__item" data-aos="fade-up" data-aos-delay="1200">
+            <img src="@/assets/images/icons/home/flexible.svg" alt="" class="homepage__big_blocks__item__icon">
+            <h3 class="homepage__big_blocks__item__title">Flexible</h3>
             <p class="homepage__big_blocks__item__text">
               A simple, fast, and safe way for all geodata users to search for and acquire commercial geospatial data assets, regardless of their sector and size.
             </p>
           </div>
-          <div class="homepage__big_blocks__item" data-aos="fade-up" data-aos-delay="300">
-            <img src="@/assets/images/icons/quality.svg" alt="" class="homepage__big_blocks__item__icon">
-            <h3 class="homepage__big_blocks__item__title">Increased quality</h3>
+          <div class="homepage__big_blocks__item" data-aos="fade-up" data-aos-delay="1500">
+            <img src="@/assets/images/icons/home/fair.svg" alt="" class="homepage__big_blocks__item__icon">
+            <h3 class="homepage__big_blocks__item__title">Fair, as in fair</h3>
             <p class="homepage__big_blocks__item__text">
-              High-quality geospatial assets and transparent licencing agreements that put an end to vendor lock-in and scaling barriers. Take advantage of efficient, scalable products and business models across all sectors!
+             All asset suppliers and their data are treated equally and under the same terms.No special favors here.
             </p>
           </div>
-          <div class="homepage__big_blocks__item" data-aos="fade-up" data-aos-delay="600">
-            <img src="@/assets/images/icons/unlock.svg" alt="" class="homepage__big_blocks__item__icon">
-            <h3 class="homepage__big_blocks__item__title">Unlock the value of your data</h3>
+          <div class="homepage__big_blocks__item" data-aos="fade-up" data-aos-delay="1800">
+            <img src="@/assets/images/icons/home/no-trading-fees.svg" alt="" class="homepage__big_blocks__item__icon">
+            <h3 class="homepage__big_blocks__item__title">No trading fees</h3>
             <p class="homepage__big_blocks__item__text">
-              Expanded geographical and sectoral coverage that enables assets to tap into new revenue streams, synergies, and business models by harnessing their added value at minimal costs and effort.
+             We only charge you a fee for processing payments – no more. We make our money from services, not you.
             </p>
+          </div>
+          <div class="homepage__big_blocks__item" data-aos="fade-up" data-aos-delay="2100">
+            <h3 class="homepage__big_blocks__item__title">Publish your work. Sell in new territories. Grow your revenue.</h3>
+            <div class="homepage__big_blocks__btn">
+          <router-link to="/catalogue" class="btn btn--std btn--trans">EXPLORE ASSETS</router-link>
+          </div>
           </div>
         </div>
       </div>
     </section>
-    <section class="homepage__assets" data-aos="fade-in">
+     <section class="homepage__big_blocks">
+         <h4 class="homepage__big_blocks__title" data-aos="fade-up">Discover, purchase and use geodata assets with topio!</h4>
+      <div class="m_container">
+        <div class="homepage__big_blocks__inner">
+          <div class="homepage__big_blocks__item" data-aos="fade-up">
+            <img src="@/assets/images/icons/home/discover-in-minutes.svg" alt="" class="homepage__big_blocks__item__icon">
+            <h3 class="homepage__big_blocks__item__title">Discover in minutes</h3>
+            <p class="homepage__big_blocks__item__text">
+              Find geospatial data and services of any type, domain, and application. All geospatial information you want in one place.
+            </p>
+          </div>
+          <div class="homepage__big_blocks__item" data-aos="fade-up" data-aos-delay="300">
+            <img src="@/assets/images/icons/home/buy.svg" alt="" class="homepage__big_blocks__item__icon">
+            <h3 class="homepage__big_blocks__item__title">Buy in seconds</h3>
+            <p class="homepage__big_blocks__item__text">
+            Found what you want? Just add to your cart, agree to the terms, pay, and get it delivered in minutes. Simple as that!
+            </p>
+          </div>
+          <div class="homepage__big_blocks__item" data-aos="fade-up" data-aos-delay="600">
+            <img src="@/assets/images/icons/home/no-surprises.svg" alt="" class="homepage__big_blocks__item__icon">
+            <h3 class="homepage__big_blocks__item__title">No surprises here</h3>
+            <p class="homepage__big_blocks__item__text">
+           Know what you buy, before you buy it. Rich profiling information and clear terms for all assets help you make the right decisions.
+            </p>
+          </div>
+           <div class="homepage__big_blocks__item" data-aos="fade-up" data-aos-delay="900">
+            <img src="@/assets/images/icons/home/data.svg" alt="" class="homepage__big_blocks__item__icon">
+            <h3 class="homepage__big_blocks__item__title">TData as a service</h3>
+            <p class="homepage__big_blocks__item__text">
+              Discover and purchase OGC services with the data you want. Pay less and as you go, use out of the box, and save time.
+            </p>
+          </div>
+           <div class="homepage__big_blocks__item" data-aos="fade-up" data-aos-delay="1200">
+            <img src="@/assets/images/icons/home/high-quality.svg" alt="" class="homepage__big_blocks__item__icon">
+            <h3 class="homepage__big_blocks__item__title">High quality</h3>
+            <p class="homepage__big_blocks__item__text">
+             Topio assets are proprietary, produced and maintained by experts. While we all love open data, we also know nothing beats quality.
+            </p>
+          </div>
+          <div class="homepage__big_blocks__item" data-aos="fade-up" data-aos-delay="1500">
+            <img src="@/assets/images/icons/home/be-protected.svg" alt="" class="homepage__big_blocks__item__icon">
+            <h3 class="homepage__big_blocks__item__title">Be protected</h3>
+            <p class="homepage__big_blocks__item__text">
+            We guarantee that what you buy is what you get. An escrow account ensures assets are delivered, our contracts protect you, and we support you in any dispute.
+            </p>
+          </div>
+          <div class="homepage__big_blocks__item" data-aos="fade-up" data-aos-delay="1800">
+            <img src="@/assets/images/icons/home/do-more.svg" alt="" class="homepage__big_blocks__item__icon">
+            <h3 class="homepage__big_blocks__item__title">Do more</h3>
+            <p class="homepage__big_blocks__item__text">
+             Topio is more than a marketplace.  Use the data you buy and data you own right here. Store, process, visualize, integrate, and create. From a simple map, to a complex analysis, we are here.
+            </p>
+          </div>
+          <div class="homepage__big_blocks__item" data-aos="fade-up" data-aos-delay="2100">
+            <h3 class="homepage__big_blocks__item__title">Explore your potentials. Create what you dreamed of. Expand in ways you never thought of.</h3>
+            <div class="homepage__big_blocks__btn">
+          <router-link to="/catalogue" class="btn btn--std btn--trans">EXPLORE ASSETS</router-link>
+          </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="homepage__assets" data-aos="fade-in" data-aos-delay="500">
       <h4 class="homepage__assets__title">Popular Assets</h4>
       <div class="a_carousel draggable">
         <a href="" class="a_carousel__item a_carousel__item--green">
@@ -156,7 +253,20 @@
         </a>
       </div>
       <div class="homepage__assets__btn">
-        <router-link to="/catalogue" class="btn btn--std btn--blue">EXPLORE ASSETS</router-link>
+        <router-link to="/catalogue" class="btn btn--std btn--blue">ALL ASSETS</router-link>
+      </div>
+    </section>
+      <section class="homepage__video">
+      <div class="m_container">
+        <div class="homepage__video__placeholder" data-aos="fade-up" data-aos-delay="400">
+          <div class="homepage__video__overlay" :class="[isClicked ? 'hidden' : 'shownk']" @click.once="videoClicked()">
+          <button class="homepage__video__button"><img src="@/assets/images/icons/home/video-btn.svg"/></button>
+          <img src="@/assets/images/home/video-thumb.jpg" class="homepage__video__thumbnail"/>
+          </div>
+           <video class="homepage__video__player" controls id="video">
+            <source src="@/assets/videos/sample-video.mp4" type="video/mp4">
+          </video>
+        </div>
       </div>
     </section>
     <section class="homepage__small_blocks">
@@ -179,7 +289,6 @@
             <img src="@/assets/images/icons/trust.svg" alt="" class="homepage__small_blocks__item__icon">
             <p class="homepage__small_blocks__item__text">Unique infrastructure that embraces traceability and auditability in favor of trust and safety</p>
           </div>
-
         </div>
       </div>
     </section>
@@ -202,9 +311,14 @@ import slider from '@/assets/scripts/slider';
 export default class Header extends Vue {
   searchResultsActive = false;
 
+  isClicked = false;
+
+  videoElem;
+
   mounted(): void {
     this.initAOS();
     this.carouselLeftOffset();
+    this.videoElem = document.getElementById('video');
     slider.init();
     window.addEventListener('resize', this.carouselLeftOffset);
   }
@@ -225,6 +339,11 @@ export default class Header extends Vue {
     const widthCont = $('.s_container')[0];
     const offset = (window.innerWidth - widthCont.getBoundingClientRect().width) / 2;
     document.documentElement.style.setProperty('--container-s-left-space', `${offset}px`);
+  }
+
+  videoClicked(): void {
+    this.isClicked = true;
+    this.videoElem.play();
   }
 }
 </script>
