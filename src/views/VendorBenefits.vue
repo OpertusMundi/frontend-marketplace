a84c0bb4ce577816d52a4e98a0653efc687fbdab
Tony Durden <antonis@bracketdev.com>
2021-06-23 18:19:22 +0300

    23-06-2021

--- src/views/VendorBenefits.vue
+++ src/views/VendorBenefits.vue
@@ -1,112 +1,25 @@
 <template>
-  <div class="page page--nooverflow">
+  <div class="page page--nooverflow" v-if="loaded">
     <div class="vendors-container">
     <section class="page__hero">
       <div class="page__hero__inner">
-        <h1>Publish. Sell. Grow.</h1>
-        <h2>Being and earning as a data supplier is made accessible and easy.</h2>
+          <h1>{{ pagedata[0].acf.heading }}</h1>
+        <h2>{{ pagedata[0].acf.subheading }}</h2>
       </div>
     </section>
    <section class="benefits">
-     <div class="benefits__row">
+     <div class="benefits__row" v-for="item, index in benefits" :key="item.id">
        <div class="benefits__text" data-aos="fade-up">
-         <h3>Become a topio supplier</h3>
-            <p>It is completely free, with no-strings attached! First, you just need to register in Topio as a standard user – while you are there, take a look around and check how users discover data to buy. It would be nice to be part of this, right?</p>
-            <router-link to="/">SHOW MORE</router-link>
+         <span>{{ index }} + {{item.show}}</span>
+        <div class="benefits__text__rendered"  v-html="item.description_short"></div>
+         <transition name="fade" mode="out-in">
+        <div class="benefits__text__rendered" v-html="item.description_full" v-show="item.show"></div>
+         </transition>
+         <div v-if="item.description_full.length" @click="getItemId(index)" class="expand-btn">{{item.show ? 'show less' : 'show more' }}</div>
        </div>
        <div class="benefits__img" data-aos="fade-left">
-         <img src="@/assets/images/vendors_benefits/vendors-supplier.svg"/>
+         <img :src="item.image.url"/>
        </div>
-     </div>
-      <div class="benefits__row">
-        <div class="benefits__img" data-aos="fade-right">
-         <img src="@/assets/images/vendors_benefits/vendors-publish.svg"/>
-       </div>
-       <div class="benefits__text" data-aos="fade-up">
-         <h3>Publish your data (and services)</h3>
-            <p>You can start trading your data in Topio within minutes - literally! The process is simple, flexible, and designed to cater to your needs. You can trade all kinds of data (tabular, vector, raster) and services (OGC-compatible, proprietary APIs) in Topio. Regardless of size, type, or domain, if your asset contains geospatial information, it has a place here.</p>
-            <router-link to="/">SHOW MORE</router-link>
-        </div>
-     </div>
-     <div class="benefits__row">
-       <div class="benefits__text" data-aos="fade-up">
-         <h3>Set your terms</h3>
-            <p>Topio provides you with all the flexibility you want for establishing your own legally binding terms and conditions when you trade your assets, in a manner that empowers and protects you. It’s just that you do not have to reinvent the wheel every time you trade with a new partner or across the EU.</p>
-            <router-link to="/">SHOW MORE</router-link>
-       </div>
-       <div class="benefits__img" data-aos="fade-left">
-         <img src="@/assets/images/vendors_benefits/vendors-terms.svg"/>
-       </div>
-     </div>
-     <div class="benefits__row">
-        <div class="benefits__img" data-aos="fade-right">
-         <img src="@/assets/images/vendors_benefits/vendors-price.svg"/>
-       </div>
-       <div class="benefits__text" data-aos="fade-up">
-         <h3>Set your price</h3>
-            <p>You have full control over the price of your asset, and of course change it anytime you time you want. We provide you with standardized options from which to choose, and clear-cut fees. So, you will know in advance exactly how much you will earn per sale – no surprises!</p>
-            <router-link to="/">SHOW MORE</router-link>
-        </div>
-     </div>
-     <div class="benefits__row">
-       <div class="benefits__text" data-aos="fade-up">
-         <h3>Automatic contracts</h3>
-            <p>When you publish an asset in Topio, all the terms, restrictions, and pricing models you have defined are used to automatically generate a contract template, which customers accept, and digitally sign, to purchase your asset.</p>
-            <router-link to="/">SHOW MORE</router-link>
-       </div>
-       <div class="benefits__img" data-aos="fade-left">
-         <img src="@/assets/images/vendors_benefits/vendors-contracts.svg"/>
-       </div>
-     </div>
-      <div class="benefits__row">
-        <div class="benefits__img" data-aos="fade-right">
-         <img src="@/assets/images/vendors_benefits/vendors-paid.svg"/>
-       </div>
-       <div class="benefits__text" data-aos="fade-up">
-         <h3>Get paid, on time</h3>
-            <p>Topio is new, but also familiar. Just like every other marketplace, clients pay digitally, in a secure and safe manner. Did we mention that Topio has zero commission for all sales of data assets? </p>
-            <router-link to="/">SHOW MORE</router-link>
-        </div>
-     </div>
-     <div class="benefits__row">
-       <div class="benefits__text" data-aos="fade-up">
-         <h3>Trust us with your data</h3>
-            <p>You have the option of securely uploading your data in Topio, allowing us to deliver it to clients after purchase. This not only makes your asset more appealing to prospective clients, but also allows us to automatically process the transaction and reimburse you in minutes. We know this might be a new concept but hear us out.</p>
-            <router-link to="/">SHOW MORE</router-link>
-       </div>
-       <div class="benefits__img" data-aos="fade-right">
-         <img src="@/assets/images/vendors_benefits/vendors-trust.svg"/>
-       </div>
-     </div>
-      <div class="benefits__row">
-        <div class="benefits__img" data-aos="fade-right">
-         <img src="@/assets/images/vendors_benefits/vendors-get-data.svg"/>
-       </div>
-       <div class="benefits__text" data-aos="fade-up">
-         <h3>Get more from your data</h3>
-            <p>Your data has more value than you think, more than you can currently harness. Do not let it get stale. Do not let your size or know-how get in the way. Do not let technology or investments prohibit you from competing.</p>
-            <router-link to="/">SHOW MORE</router-link>
-        </div>
-     </div>
-      <div class="benefits__row">
-       <div class="benefits__text" data-aos="fade-up">
-         <h3>You are in control</h3>
-            <p>Topio provides you with all the tools you need to manage your assets, monitor your transactions, keep track of your payments, and gain insights over how your assets perform. All within a free, simple to use, personalized, and secure Dashboard integrated in Topio. Everything in one place, and for all of our suppliers.</p>
-            <router-link to="/">SHOW MORE</router-link>
-       </div>
-       <div class="benefits__img" data-aos="fade-left">
-         <img src="@/assets/images/vendors_benefits/vendors-control.svg"/>
-       </div>
-     </div>
-     <div class="benefits__row">
-        <div class="benefits__img" data-aos="fade-right">
-         <img src="@/assets/images/vendors_benefits/vendors-eu.svg"/>
-       </div>
-       <div class="benefits__text" data-aos="fade-up">
-         <h3>An EU market</h3>
-            <p>Topio is a digital marketplace made in the EU. As such, we are particularly diligent in all matters relating to regulatory conformance, adherence to legal frameworks, fairness, and security. This is to protect fair trade, and to protect you.</p>
-            <router-link to="/">SHOW MORE</router-link>
-        </div>
      </div>
    </section>
     </div>
@@ -183,12 +96,39 @@
 import { Component, Vue } from 'vue-property-decorator';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
+import axios from 'axios';
 
 @Component
 export default class VendorBenefits extends Vue {
+  pagedata: any;
+
+  wpUrl: string;
+
+  loaded: boolean;
+
+  show: boolean;
+
+  benefits: any;
+
+  showIndex: any;
+
+  constructor() {
+    super();
+
+    this.pagedata = null;
+    this.loaded = false;
+    this.show = false;
+    this.benefits = [];
+    this.showIndex = null;
+    this.wpUrl = this.$store.getters.getConfig.configuration.wordPress.endpoint;
+  }
+
   mounted():void {
     this.initAOS();
-    this.headerStatic();
+  }
+
+  created():void {
+    this.getPageData();
   }
 
   initAOS = (): void => {
@@ -198,8 +138,26 @@ export default class VendorBenefits extends Vue {
     });
   }
 
-  headerStatic = (): void => {
-    console.log(this.$root);
+  getPageData(): void {
+    axios.get(`${this.wpUrl}/wp-json/wp/v2/pages?slug=vendor-benefits`).then((response) => {
+      this.pagedata = response.data;
+      this.benefits = response.data[0].acf.benefits;
+      this.benefits.forEach((element) => {
+        this.$set(element, 'show', false);
+      });
+      console.log(this.benefits);
+    }).catch((error) => {
+      console.log(error);
+    }).finally(() => {
+      this.loaded = true;
+      console.log('data fetching completed');
+    });
+  }
+
+  getItemId(value): void {
+    this.benefits[value].show = !this.benefits[value].show;
+    // this.$set(this.benefits.show, value, true);
+    // console.log(this.benefits[value].show);
   }
 }
 </script>
