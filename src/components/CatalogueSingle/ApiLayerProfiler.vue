cf0c486f814e2bbf9e955e0520d5fd46b1e7ddd5
treecon <kon3dos@gmail.com>
2021-09-02 15:03:26 +0300

    service asset view (Layer Profiler, Metadata)

---
+++ src/components/CatalogueSingle/ApiLayerProfiler.vue
@@ -0,0 +1,330 @@
+<template>
+  <section class="asset__section" :class="{'asset__section--expanded': isExpanded}">
+
+    <!-- MODALS -->
+    <modal :withSlots="true" :show="modalToShow === 'modalShowCrsList'" @dismiss="modalToShow = ''" :modalId="'modalShowCrsList'" :showCancelButton="false" :showCloseButton="false">
+      <template v-slot:body>
+        <div class="p-xs-30">
+          <h4>CRS list</h4>
+          <div style="max-height: 300px; overflow-y: auto">
+            <ul style="text-align: left">
+              <li v-for="crs in resources[1].crs" :key="crs">{{ crs }}</li>
+            </ul>
+          </div>
+        </div>
+      </template>
+
+      <template v-slot:footer>
+        <button class="btn btn--std btn--blue ml-xs-20" @click="modalToShow = ''">OK</button>
+      </template>
+    </modal>
+
+    <modal :withSlots="true" :show="modalToShow === 'modalShowOutputFormatsList'" @dismiss="modalToShow = ''" :modalId="'modalShowOutputFormatsList'" :showCancelButton="false" :showCloseButton="false">
+      <template v-slot:body>
+        <div class="p-xs-30">
+          <h4>All output formats</h4>
+          <div style="max-height: 300px; overflow-y: auto">
+            <ul style="text-align: left">
+              <li v-for="format in resources[1].outputFormats" :key="format">{{ format }}</li>
+            </ul>
+          </div>
+        </div>
+      </template>
+
+      <template v-slot:footer>
+        <button class="btn btn--std btn--blue ml-xs-20" @click="modalToShow = ''">OK</button>
+      </template>
+    </modal>
+    <!-- END OF MODALS -->
+
+    <div class="asset__section__head">
+      <div class="d-flex space-between">
+        <h4>Layer Profiler</h4>
+        <div @click="toggleExpansion" style="cursor: pointer"><svg data-name="Full screen icon" xmlns="http://www.w3.org/2000/svg" width="15.989" height="16"><path data-name="Path 9453" d="m15.187 0 .8.8v3.763h-1.346v-1.73l.093-.453-.1-.058-.29.372-3.24 3.24L10.07 4.9l3.229-3.252.372-.29-.058-.1-.453.093h-1.73V0z" fill="#333"/><path data-name="Path 9452" d="m.801 0-.8.8v3.765h1.346v-1.73l-.093-.453.1-.058.29.372 3.245 3.239 1.034-1.034-3.229-3.252-.372-.29.058-.1.453.093h1.731V0z" fill="#333"/><path data-name="Path 9451" d="m.801 16-.8-.8v-3.764h1.346v1.73l-.093.453.1.058.29-.372 3.24-3.24L5.923 11.1l-3.229 3.251-.372.29.058.1.453-.093h1.731V16z" fill="#333"/><path data-name="Path 9450" d="m15.187 16 .8-.8v-3.76h-1.346v1.73l.093.453-.1.058-.29-.372-3.24-3.24-1.034 1.034 3.229 3.252.372.29-.058.1-.453-.093h-1.73v1.347z" fill="#333"/></svg></div>
+      </div>
+
+      <!-- <div class="asset__section__head__sample_download" v-if="isUserAuthenticated && metadata.samples">
+        <span><strong>Select layer:</strong></span>
+        <multiselect v-model="metadataDownloadFileSelection" :options="['file_1']" :allowEmpty="false" :preselectFirst="true" :searchable="false" :openDirection="'bottom'" :close-on-select="true" :show-labels="false" placeholder="Select a sample to download"></multiselect>
+        <div v-if="metadataDownloadFileSelection" @click="onDownloadAutomatedMetadata" class="asset__section__head__sample_download__btn"><svg data-name="Group 2342" xmlns="http://www.w3.org/2000/svg" width="15" height="16"><g data-name="Group 753"><g data-name="Group 752"><path data-name="Path 2224" d="M11.455 7.293A.5.5 0 0 0 11.002 7h-2V.5a.5.5 0 0 0-.5-.5h-2a.5.5 0 0 0-.5.5V7h-2a.5.5 0 0 0-.376.829l3.5 4a.5.5 0 0 0 .752 0l3.5-4a.5.5 0 0 0 .077-.536z" fill="#333"/></g></g><g data-name="Group 755"><g data-name="Group 754"><path data-name="Path 2225" d="M13 11v3H2v-3H0v4a1 1 0 0 0 1 1h13a1 1 0 0 0 1-1v-4z" fill="#333"/></g></g></svg></div>
+      </div> -->
+
+      <div class="asset__section__head__main_information asset__section__head__main_information--includes-buttons" v-if="isUserAuthenticated">
+        <p v-if="resources[1].crs.length < 5"><strong>CRS:</strong> {{ resources[1].crs.join(', ') }}</p>
+        <p v-else><strong>CRS:</strong> {{ resources[1].crs.slice(0, 5).join(', ') }}... <button @click="onShowWholeCrsList" class="btn btn--std btn--outlinedark">show all</button></p>
+        <p><strong>ATTRIBUTION:</strong> {{ resources[1].attribution }}</p>
+        <p><strong>TYPE:</strong> {{ resources[1].serviceType }}</p>
+      </div>
+
+      <a href="#" class="asset__section__head__toggle"><img src="@/assets/images/icons/arrow_down.svg" alt=""></a>
+      <ul class="asset__section__head__tabs" v-if="isUserAuthenticated">
+        <li><a href="#" @click.prevent="activeTab = 1" :class="{ 'active' : activeTab == 1 }">COVERAGE</a></li>
+        <li><a href="#" @click.prevent="activeTab = 2" :class="{ 'active' : activeTab == 2 }">DETAILS</a></li>
+        <li><a href="#" @click.prevent="activeTab = 3" :class="{ 'active' : activeTab == 3 }">STYLES</a></li>
+        <li><a href="#" @click.prevent="activeTab = 4" :class="{ 'active' : activeTab == 4 }">DIMENSIONS</a></li>
+        <li><a href="#" @click.prevent="activeTab = 5" :class="{ 'active' : activeTab == 5 }">TILES</a></li>
+        <li><a href="#" @click.prevent="activeTab = 6" :class="{ 'active' : activeTab == 6 }">SAMPLE 1</a></li>
+        <li><a href="#" @click.prevent="activeTab = 7" :class="{ 'active' : activeTab == 7 }">SAMPLE 2</a></li>
+        <li><a href="#" @click.prevent="activeTab = 8" :class="{ 'active' : activeTab == 8 }">SAMPLE 3</a></li>
+        <!-- <li><a href="#" @click.prevent="activeTab = 3" :class="{ 'active' : activeTab == 3 }">Sample 1</a></li> -->
+        <!-- <li><a href="#" @click.prevent="activeTab = 4" :class="{ 'active' : activeTab == 4 }">Sample 2</a></li> -->
+      </ul>
+    </div>
+    <div class="asset__section__content">
+      <div class="asset__section__content__inner asset__section__content__inner--centered" v-if="!isUserAuthenticated">
+        <div class="m-xs-40">
+          <h2>Automated metadata and samples are visible only to registered users</h2>
+          <p>Create a topio user account and explore geodata without limitations</p>
+          <a href="/register" class="btn--std btn--blue">REGISTER</a>
+        </div>
+      </div>
+      <div class="asset__section__content__inner" v-else>
+        <ul class="asset__section__tabs asset__section__tabs__service_details">
+
+          <!-- COVERAGE -->
+          <li v-if="activeTab == 1 && resources[1].bbox">
+            <p>Contains visualisation of the coverage on a map</p>
+            <div class="tab_maps-map">
+              <l-map
+                ref="mapMbr"
+                :bounds="getMapBoundsFromGeoJson(resources[1].bbox)"
+                :maxBounds="getMapBoundsFromGeoJson(resources[1].bbox)"
+                :options="mapConfig.options"
+              >
+                <l-tile-layer
+                :url="mapConfig.tilesUrl"
+                :attribution="mapConfig.attribution"/>
+                <l-geo-json
+                  :geojson="resources[1].bbox"
+                  :optionsStyle="{color: 'orange'}"
+                >
+                </l-geo-json>
+              </l-map>
+            </div>
+          </li>
+
+          <!-- DETAILS -->
+          <li v-if="activeTab == 2">
+            <p>Details of the service</p>
+            <hr>
+            <h3>General</h3>
+            <div class="asset__section__tabs__info_table asset__section__tabs__info_table--includes-buttons">
+              <p><strong>Identifier</strong>{{ resources[1].id }}</p>
+              <p><strong>Scale</strong>{{ `1:${resources[1].maxScale}` }} - {{ `1:${resources[1].minScale}` }}</p>
+              <p v-if="resources[1].outputFormats.length < 3"><strong>Output formats</strong> {{ resources[1].outputFormats.join(', ') }}</p>
+              <p v-else><strong>Output formats</strong> <span>{{ resources[1].outputFormats.slice(0, 3).join(', ') }}... <button @click="onShowWholeOutputFormatsList" class="btn btn--std btn--outlinedark">show all</button></span></p>
+            </div>
+            <hr>
+            <h3>Attributes</h3>
+            <div v-if="resources[1].attributes" class="asset__section__tabs__info_table asset__section__tabs__info_table--includes-buttons">
+              <p><strong>queryable</strong><span>{{ resources[1].attributes.queryable }}</span></p>
+              <p><strong>cascaded</strong><span>{{ resources[1].attributes.cascaded }}</span></p>
+              <p><strong>opaque</strong><span>{{ resources[1].attributes.opaque }}</span></p>
+              <p><strong>noSubsets</strong><span>{{ resources[1].attributes.noSubsets }}</span></p>
+              <p><strong>fixedWidth</strong><span>{{ resources[1].attributes.fixedWidth }}</span></p>
+              <p><strong>fixedHeight</strong><span>{{ resources[1].attributes.fixedHeight }}</span></p>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+    <a href="#" class="asset__section__toggle"><img src="@/assets/images/icons/arrow_down.svg" alt=""></a>
+  </section>
+</template>
+<script lang="ts">
+import {
+  Vue,
+  Component,
+  Prop,
+  Watch,
+} from 'vue-property-decorator';
+import store from '@/store';
+import { Chart } from 'highcharts-vue';
+import Multiselect from 'vue-multiselect';
+import L from 'leaflet';
+import {
+  LMap, LTileLayer, LPolygon, LGeoJson,
+} from 'vue2-leaflet';
+import 'leaflet/dist/leaflet.css';
+import { parse as wktToGeojsonParser } from 'wellknown';
+import CatalogueApi from '@/service/catalogue';
+import DraftAssetApi from '@/service/draft';
+import {
+  CatalogueItemDetails,
+} from '@/model/catalogue';
+// eslint-disable-next-line
+import { GeoJsonObject } from 'geojson';
+import Modal from '@/components/Modal.vue';
+import { bbox as turfBBox } from '@turf/turf';
+import { FileResource, Resource, ServiceResource } from '@/model/asset';
+
+@Component({
+  components: {
+    Chart,
+    Multiselect,
+    LMap,
+    LTileLayer,
+    LPolygon,
+    LGeoJson,
+    Modal,
+  },
+})
+export default class ApiLayerProfiler extends Vue {
+  @Prop({ required: true }) private catalogueItem!: CatalogueItemDetails;
+
+  @Prop() private mode?: string;
+
+  @Prop() private assetKey?: string;
+
+  resources: (Resource | FileResource | ServiceResource)[];
+
+  hiddenMetadata: string[] | undefined;
+
+  catalogueApi: CatalogueApi;
+
+  draftAssetApi: DraftAssetApi;
+
+  activeTab: number;
+
+  isUserAuthenticated: boolean;
+
+  mapConfig: any;
+
+  isExpanded: boolean;
+
+  modalToShow: string;
+
+  constructor() {
+    super();
+
+    this.catalogueApi = new CatalogueApi();
+    this.draftAssetApi = new DraftAssetApi();
+
+    this.resources = this.catalogueItem.resources;
+
+    this.activeTab = 1;
+
+    this.isExpanded = false;
+
+    this.modalToShow = '';
+
+    this.isUserAuthenticated = store.getters.isAuthenticated;
+
+    this.mapConfig = {
+      options: {
+        minZoom: 1,
+        maxZoom: 10,
+      },
+      styleHeatmap: (feature) => ({
+        fillColor: feature.properties.fill,
+        color: feature.properties.fill,
+        fillOpacity: 0.7,
+        opacity: 0.4,
+        weight: 1,
+        stroke: true,
+      }),
+      tilesUrl: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+      attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+    };
+  }
+
+  mounted(): void {
+    console.log('resources', this.resources);
+  }
+
+  @Watch('activeTab', { immediate: true })
+  onActiveTabChange(activeTab: number): void {
+    // if tab is maps-tab
+    if (activeTab === 1 && this.isUserAuthenticated) {
+      this.setMinMaxZoomLevels();
+    }
+  }
+
+  toggleExpansion(): void {
+    // if (!this.isExpanded) window.scrollTo(0, 0);
+    this.isExpanded = !this.isExpanded;
+    document.body.style.overflowY = this.isExpanded ? 'hidden' : 'visible';
+  }
+
+  setMinMaxZoomLevels(): void {
+    this.$nextTick(() => {
+      const fitBoundsZoomLevel = (this as any).$refs.mapMbr.mapObject.getBoundsZoom(L.geoJSON((this.resources[1] as ServiceResource).bbox as GeoJsonObject).getBounds());
+
+      const zoomOffset = 2;
+
+      const minZoom = fitBoundsZoomLevel - zoomOffset < 0 ? 0 : fitBoundsZoomLevel - zoomOffset;
+      const maxZoom = fitBoundsZoomLevel + zoomOffset;
+
+      console.log('minZoom', minZoom, 'maxZoom', maxZoom);
+
+      Vue.set(this.mapConfig.options, 'minZoom', minZoom);
+      Vue.set(this.mapConfig.options, 'maxZoom', maxZoom);
+    });
+  }
+
+  onShowWholeCrsList(): void {
+    this.modalToShow = 'modalShowCrsList';
+  }
+
+  onShowWholeOutputFormatsList(): void {
+    this.modalToShow = 'modalShowOutputFormatsList';
+  }
+
+  wktToGeoJson(wkt: string): any {
+    return wktToGeojsonParser(wkt);
+  }
+
+  // mbrToLeafletBounds(wkt: string): number[][] {
+  //   const geoJson = wktToGeojsonParser(wkt);
+  //   const bounds = geoJson.coordinates[0]
+  //     .map((x) => [x[1], x[0]]);
+  //   return bounds;
+  // }
+
+  // getMapBoundsFromWKT(wkt: string): number[][] {
+  //   const geoJson = wktToGeojsonParser(wkt);
+  //   const bbox = turfBBox(geoJson);
+  //   return [[bbox[1], bbox[0]], [bbox[3], bbox[2]]];
+  // }
+
+  getMapBoundsFromGeoJson(geoJson: GeoJsonObject): number[][] {
+    const bbox = turfBBox(geoJson);
+    return [[bbox[1], bbox[0]], [bbox[3], bbox[2]]];
+  }
+}
+</script>
+<style lang="scss">
+  @import "@/assets/styles/_assets.scss";
+  @import "~vue-multiselect/dist/vue-multiselect.min.css";
+  @import "@/assets/styles/graphs/_table.scss";
+  @import "@/assets/styles/abstracts/_spacings.scss";
+  @import "@/assets/styles/abstracts/_flexbox-utilities.scss";
+  @import "~flexboxgrid/css/flexboxgrid.min.css";
+
+  // LEAFLET FIXES DUE TO STYLE OVERRIDE
+  .leaflet-container a {
+    -webkit-tap-highlight-color: rgba(51, 181, 229, 0.4) !important;
+  }
+  .leaflet-container a {
+    color: #0078A8 !important;
+  }
+  .leaflet-touch .leaflet-bar a {
+    width: 30px !important;
+    height: 30px !important;
+    line-height: 30px !important;
+  }
+  .leaflet-control-attribution, .leaflet-control-attribution a {
+    font-size: 11px !important;
+  }
+  .leaflet-control-attribution a {
+    text-decoration: none !important;
+  }
+  .leaflet-bar > a:link {
+    color: black !important;
+    text-decoration: none !important;
+    font-size: 22px !important;
+    width: 30px;
+    height: 30px;
+    line-height: 30px;
+  }
+</style>
---
+++ src/components/CatalogueSingle/ApiMetadata.vue
@@ -0,0 +1,120 @@
+<template>
+  <section class="asset__section">
+    <div class="asset__section__head">
+      <h4>Metadata</h4>
+      <a href="#" class="asset__section__head__toggle"><img src="@/assets/images/icons/arrow_down.svg" alt=""></a>
+      <ul class="asset__section__head__tabs">
+        <li><a href="#" @click.prevent="activeTab = 1" :class="{ 'active' : activeTab == 1 }">Identification</a></li>
+        <li><a href="#" @click.prevent="activeTab = 2" :class="{ 'active' : activeTab == 2 }">Classification</a></li>
+        <li><a href="#" @click.prevent="activeTab = 3" :class="{ 'active' : activeTab == 3 }">Geography</a></li>
+        <li><a href="#" @click.prevent="activeTab = 4" :class="{ 'active' : activeTab == 4 }">Temporal Reference</a></li>
+        <li><a href="#" @click.prevent="activeTab = 5" :class="{ 'active' : activeTab == 5 }">Conformity & Lineage</a></li>
+        <li><a href="#" @click.prevent="activeTab = 6" :class="{ 'active' : activeTab == 6 }">More</a></li>
+      </ul>
+    </div>
+    <div class="asset__section__content">
+      <div class="asset__section__content__inner">
+        <ul class="asset__section__tabs">
+          <li v-if="activeTab == 1 && catalogueItem">
+            <div class="asset__section__tabs__info_table">
+              <p><strong>Title:</strong><span>{{ catalogueItem.title }}</span></p>
+              <p><strong>Abstract:</strong><span>{{ catalogueItem.abstract }}</span></p>
+              <p><strong>Type:</strong><span>{{ catalogueItem.type }}</span></p>
+              <p><strong>Format:</strong><span>{{ catalogueItem.spatialDataServiceType }}</span></p>
+              <p><strong>Language:</strong><span>{{ $store.getters.getConfig.configuration.europeLanguages.find((x) => x.code === catalogueItem.language).name }}</span></p>
+              <p><strong>Resource locator:</strong><span>{{ catalogueItem.resourceLocator }}</span></p>
+              <p><strong>Version:</strong><span>{{ catalogueItem.version }}</span></p>
+              <!-- <p><strong>Data coupled resource</strong><span>{{  }}</span></p> -->
+              <p><strong>Parent identifier (PID):</strong><span>{{ catalogueItem.parentId }}</span></p>
+            </div>
+          </li>
+          <li v-if="activeTab == 2">
+             <div class="asset__section__tabs__info_table">
+              <p><strong>Keywords:</strong><span>{{ catalogueItem.keywords.map((x) => x.keyword).join(', ') }}</span></p>
+              <p><strong>Topic Category:</strong><span>{{ catalogueItem.topicCategory.join(', ') }}</span></p>
+            </div>
+          </li>
+          <li v-if="activeTab == 3">
+            <div class="asset__section__tabs__info_table">
+              <p><strong>Reference system:</strong><span>{{ crsLabel }}</span></p>
+              <p><strong>Spatial resolution:</strong><span>{{ catalogueItem.spatialResolution }}</span></p>
+              <p><strong>Scale:</strong><span>{{ catalogueItem.scales.map((x) => x.description).join(', ') }}</span></p>
+            </div>
+          </li>
+          <li v-if="activeTab == 4">
+            <div class="asset__section__tabs__info_table">
+              <p><strong>Date start:</strong><span>{{ formatDate(catalogueItem.dateStart) }}</span></p>
+              <p><strong>Date end:</strong><span>{{ formatDate(catalogueItem.dateEnd) }}</span></p>
+              <p><strong>Creation date:</strong><span>{{ formatDate(catalogueItem.creationDate) }}</span></p>
+              <p><strong>Publication date:</strong><span>{{ formatDate(catalogueItem.publicationDate) }}</span></p>
+              <p><strong>Revision date:</strong><span>{{ formatDate(catalogueItem.revisionDate) }}</span></p>
+            </div>
+          </li>
+          <li v-if="activeTab == 5">
+            <div class="asset__section__tabs__info_table">
+              <p><strong>Conformity:</strong><span>{{ catalogueItem.conformity }}</span></p>
+              <p><strong>Lineage:</strong><span>{{ catalogueItem.lineage }}</span></p>
+            </div>
+          </li>
+          <li v-if="activeTab == 6">
+            <div class="asset__section__tabs__info_table">
+              <p><strong>Publisher name:</strong><span>{{ catalogueItem.publisherName }}</span></p>
+              <p><strong>Publisher E-mail:</strong><span>{{ catalogueItem.publisherEmail }}</span></p>
+              <!-- <p><strong>Additional resources:</strong><span>{{ catalogueItem.additionalResources }}</span></p> -->
+              <p><strong>Public access limitations:</strong><span>{{ catalogueItem.publicAccessLimitations }}</span></p>
+              <p><strong>Metadata language:</strong><span>{{ $store.getters.getConfig.configuration.europeLanguages.find((x) => x.code === catalogueItem.metadataLanguage).name }}</span></p>
+              <p><strong>Metadata version:</strong><span>{{ catalogueItem.version }}</span></p>
+              <p><strong>Metadata date:</strong><span>{{ formatDate(catalogueItem.metadataDate) }}</span></p>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+    <a href="#" class="asset__section__toggle"><img src="@/assets/images/icons/arrow_down.svg" alt=""></a>
+  </section>
+</template>
+<script lang="ts">
+import { Vue, Component, Prop } from 'vue-property-decorator';
+import { CatalogueItem } from '@/model';
+import SpatialApi from '@/service/spatial';
+import moment from 'moment';
+
+@Component
+export default class TermsAndRestrictions extends Vue {
+  @Prop({ required: true }) private catalogueItem!: CatalogueItem;
+
+  spatialApi: SpatialApi;
+
+  activeTab: number;
+
+  crsLabel: string;
+
+  constructor() {
+    super();
+
+    this.spatialApi = new SpatialApi();
+
+    console.log('abc', this.catalogueItem);
+
+    this.activeTab = 1;
+
+    this.crsLabel = '';
+  }
+
+  created(): void {
+    if (this.catalogueItem.referenceSystem) {
+      this.spatialApi.getEpsgCodes(this.catalogueItem.referenceSystem).then((epsgResponse) => {
+        const epsg = epsgResponse.result.find((x) => `${x.code}` === this.catalogueItem.referenceSystem);
+        if (epsg) this.crsLabel = `EPSG:${epsg.code} | ${epsg.name}`;
+      });
+    }
+  }
+
+  formatDate(date: string): string {
+    return moment(date).format('DD MMM YYYY');
+  }
+}
+</script>
+<style lang="scss">
+  @import "@/assets/styles/_assets.scss";
+</style>
