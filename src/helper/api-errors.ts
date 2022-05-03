4cd191ddb82cdbe92619df4d36067659ed455bd5
treecon <kon3dos@gmail.com>
2022-05-03 19:32:53 +0300

    show api-error modal only for specific roles

--- src/helper/api-errors.ts
+++ src/helper/api-errors.ts
@@ -1,9 +1,12 @@
 import { ServerMessage } from '@/model';
+import { EnumRole } from '@/model/role';
 import store from '@/store';
 
 const showApiErrorModal = (messages: ServerMessage[]): void => {
-  store.commit('setErrorMessages', messages);
-  store.commit('setShownGlobalModal', 'modalApiError');
+  if (store.getters.hasRole([EnumRole.ROLE_ADMIN, EnumRole.ROLE_DEVELOPER])) {
+    store.commit('setErrorMessages', messages);
+    store.commit('setShownGlobalModal', 'modalApiError');
+  }
   store.commit('setLoading', false);
 };
 
