a03c051ec44be821b90b4bef66ac9bea10ea3d2b
treecon <kon3dos@gmail.com>
2022-11-21 12:13:32 +0200

    minor (labelize string)

--- src/helper/labels.ts
+++ src/helper/labels.ts
@@ -1,3 +1,3 @@
 import labels from '@/config/labels';
 
-export default (text: string, searchById = false): string => (labels.find((x) => (searchById ? x.id === text : x.text === text))?.label || 'ERR');
+export default (text: string, searchById = false): string | null => (labels.find((x) => (searchById ? x.id === text : x.text === text))?.label || null);
