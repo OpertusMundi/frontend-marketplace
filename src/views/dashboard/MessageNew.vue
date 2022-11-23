ca58de1d598308720d9809986631fe7dd9dc053e
treecon <kon3dos@gmail.com>
2022-11-23 15:30:48 +0200

    send message to helpdesk (messages > new message)

--- src/views/dashboard/MessageNew.vue
+++ src/views/dashboard/MessageNew.vue
@@ -5,37 +5,32 @@
         <h1>New Message</h1>
       </div>
     </div>
-    <div class="dashboard__form">
+    <div class="dashboard__form dashboard__form--transparent">
         <div class="single-message">
-          <div class="form-group">
-            <input type="email" class="form-group__text" id="email" placeholder="Enter email">
-            <div class="errors">Please check you input!</div>
-          </div>
-          <div class="form-group">
-            <input type="email" class="form-group__text" id="email" placeholder="Enter email">
-            <div class="errors">Please check you input!</div>
-          </div>
-          <div class="form-group">
-            <select name="issue" id="issue">
-              <option value="1">Option 1</option>
-              <option value="2">Option 2</option>
-              <option value="3">Option 3</option>
-              <option value="4">Option 4</option>
-            </select>
-            <div class="errors">Please check you input!</div>
-          </div>
-          <div class="form-group">
-            <div class="form-group__stickyright">
-              <div class="asset-owner__inner__logo">
-                <img src="@/assets/images/about/partners/athina_logo.svg" alt="">
+          <validation-observer ref="vpNewMessage">
+            <validation-provider v-slot="{ errors }" name="Name" rules="required">
+              <div class="form-group">
+                <input type="text" class="form-group__text" id="email" placeholder="Subject" v-model="subject">
+                <div class="errors" v-if="errors"><span v-for="error in errors" v-bind:key="error">{{ error }}</span></div>
               </div>
-              <p class="form-group__stickyright__text">AthenaRC</p>
+            </validation-provider>
+
+            <div class="form-group">
+              <div class="form-group__stickyright">
+                <div class="asset-owner__inner__logo">
+                  <!-- <img src="@/assets/images/about/partners/athina_logo.svg" alt=""> -->
+                  <img :src="$store.getters.getProfile.image ? 'data:' + $store.getters.getProfile.imageMimeType + ';base64, ' + $store.getters.getProfile.image : defaultLogo()" alt="user image">
+                </div>
+                <p class="form-group__stickyright__text">{{ $store.getters.getProfile.firstName }} {{ $store.getters.getProfile.lastName }}</p>
+              </div>
+              <validation-provider v-slot="{ errors }" name="Message" rules="required">
+                <textarea name="message" id="message" rows="10" placeholder="Your message" v-model="message"></textarea>
+                <div class="errors" v-if="errors"><span v-for="error in errors" v-bind:key="error">{{ error }}</span></div>
+              </validation-provider>
             </div>
-            <textarea name="" id="" cols="30" rows="10" placeholder="Your message"></textarea>
-            <div class="errors">Please check you input!</div>
-          </div>
-          <div class="form-group form-group--right">
-            <button type="submit" class="btn--std btn--dark">SEND</button>
+          </validation-observer>
+          <div class="form-group form-group--right submit-btn-container">
+            <button type="submit" class="btn--std btn--blue" @click="sendMessage">SEND</button>
           </div>
         </div>
     </div>
@@ -44,9 +39,43 @@
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
+import {
+  ValidationProvider,
+  ValidationObserver,
+  extend,
+} from 'vee-validate';
+import { required } from 'vee-validate/dist/rules';
+import MessageApi from '@/service/message';
+import getDefaultLogo from '@/helper/logo';
+import store from '@/store';
 
-@Component
+extend('required', required);
+
+@Component({
+  components: { ValidationObserver, ValidationProvider },
+})
 export default class DashboardHome extends Vue {
+  messageApi = new MessageApi();
+
+  subject = '';
+
+  message = '';
+
+  defaultLogo(): string {
+    return getDefaultLogo();
+  }
+
+  async sendMessage(): Promise<void> {
+    const valid = await (this.$refs.vpNewMessage as InstanceType<typeof ValidationObserver>).validate();
+    if (!valid) return;
+
+    store.commit('setLoading', true);
+
+    const response = await this.messageApi.sendMessageToHelpdesk(this.subject, this.message);
+
+    store.commit('setLoading', false);
+    if (response.success) this.$router.push('/dashboard/messages');
+  }
 }
 </script>
 <style lang="scss">
