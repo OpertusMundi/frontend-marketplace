8a4198484ac4ff05358b06e8a8a1c28e2b1a9234
treecon <kon3dos@gmail.com>
2022-12-13 17:06:32 +0200

    carousel-card fixes

--- src/components/Catalogue/CardIcon.vue
+++ src/components/Catalogue/CardIcon.vue
@@ -1,12 +1,12 @@
 <template>
   <div>
-    <img src="@/assets/images/icons/types/vector.svg" alt="" v-if="asset.type === 'VECTOR'">
-    <img src="@/assets/images/icons/types/raster.svg" alt="" v-if="asset.type === 'RASTER'">
-    <img src="@/assets/images/icons/types/tabular.svg" alt="" v-if="asset.type === 'TABULAR'">
-    <img src="@/assets/images/icons/types/bundle.svg" alt="" v-if="asset.type === 'BUNDLE'">
-    <img src="@/assets/images/icons/types/wms.svg" alt="" v-if="asset.spatialDataServiceType === 'WMS' || asset.serviceType === 'WMS'">
-    <img src="@/assets/images/icons/types/wfs.svg" alt="" v-if="asset.spatialDataServiceType === 'WFS' || asset.serviceType === 'WFS'">
-    <img src="@/assets/images/icons/types/data_api.svg" alt="" v-if="asset.spatialDataServiceType === 'DATA_API' || asset.serviceType === 'DATA_API'">
+    <img :height="imgHeight || ''" src="@/assets/images/icons/types/vector.svg" alt="" v-if="asset.type === 'VECTOR'">
+    <img :height="imgHeight || ''" src="@/assets/images/icons/types/raster.svg" alt="" v-if="asset.type === 'RASTER'">
+    <img :height="imgHeight || ''" src="@/assets/images/icons/types/tabular.svg" alt="" v-if="asset.type === 'TABULAR'">
+    <img :height="imgHeight || ''" src="@/assets/images/icons/types/bundle.svg" alt="" v-if="asset.type === 'BUNDLE'">
+    <img :height="imgHeight || ''" src="@/assets/images/icons/types/wms.svg" alt="" v-if="asset.spatialDataServiceType === 'WMS' || asset.serviceType === 'WMS'">
+    <img :height="imgHeight || ''" src="@/assets/images/icons/types/wfs.svg" alt="" v-if="asset.spatialDataServiceType === 'WFS' || asset.serviceType === 'WFS'">
+    <img :height="imgHeight || ''" src="@/assets/images/icons/types/data_api.svg" alt="" v-if="asset.spatialDataServiceType === 'DATA_API' || asset.serviceType === 'DATA_API'">
   </div>
 </template>
 <script lang="ts">
@@ -16,6 +16,8 @@ import { CatalogueItem } from '@/model';
 @Component
 export default class CardIcon extends Vue {
   @Prop({ required: true }) asset!: CatalogueItem;
+
+  @Prop({ required: false }) imgHeight?: number;
 }
 </script>
 <style lang="scss" scoped>
