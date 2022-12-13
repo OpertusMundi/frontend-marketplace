1e4fef1d50394b2341e971e11501c0fbe4054f50
treecon <kon3dos@gmail.com>
2022-12-13 17:12:05 +0200

    added asset type icons

--- src/components/Catalogue/CardIcon.vue
+++ src/components/Catalogue/CardIcon.vue
@@ -3,6 +3,8 @@
     <img :height="imgHeight || ''" src="@/assets/images/icons/types/vector.svg" alt="" v-if="asset.type === 'VECTOR'">
     <img :height="imgHeight || ''" src="@/assets/images/icons/types/raster.svg" alt="" v-if="asset.type === 'RASTER'">
     <img :height="imgHeight || ''" src="@/assets/images/icons/types/tabular.svg" alt="" v-if="asset.type === 'TABULAR'">
+    <img :height="imgHeight || ''" src="@/assets/images/icons/types/netcdf.svg" alt="" v-if="asset.type === 'NETCDF'">
+    <img :height="imgHeight || ''" src="@/assets/images/icons/types/eo.svg" alt="" v-if="asset.type === 'SENTINEL_HUB_OPEN_DATA'">
     <img :height="imgHeight || ''" src="@/assets/images/icons/types/bundle.svg" alt="" v-if="asset.type === 'BUNDLE'">
     <img :height="imgHeight || ''" src="@/assets/images/icons/types/wms.svg" alt="" v-if="asset.spatialDataServiceType === 'WMS' || asset.serviceType === 'WMS'">
     <img :height="imgHeight || ''" src="@/assets/images/icons/types/wfs.svg" alt="" v-if="asset.spatialDataServiceType === 'WFS' || asset.serviceType === 'WFS'">
